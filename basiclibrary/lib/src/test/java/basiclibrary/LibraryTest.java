cfa86be343302a00336690aab1117062cde726fb
Manar-Alzoubi <mzzo3bi20988@gmail.com>
2022-03-05 21:56:59 +0300

    updated

--- basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
@@ -11,6 +11,10 @@ package basiclibrary;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
@@ -19,8 +23,42 @@ class LibraryTest {
         assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
     }
 
+@Test
+void tallyTest()
+{
+    Library   classUnderTest = new Library();
+    List<String> votesTest = new ArrayList<>();
+    votesTest.add("Shrub");
+    votesTest.add("Shrub");
+    votesTest.add("Shrub");
+    votesTest.add("Hedge");
+    votesTest.add("Shrub");
+    votesTest.add("Shrub");
+    votesTest.add("Shrub");
+    String newResult = Library.tally(votesTest);
+    assertEquals("Shrub",newResult);
 
-    @Test void RollTest(){
+
+}
+
+@Test
+void minMaxTest()
+{
+    Library classUnderTest = new Library();
+    int[][] arr = {
+            {66, 64, 58, 65, 71, 57, 60},
+            {57, 65, 65, 70, 72, 65, 51},
+            {55, 54, 60, 53, 59, 57, 61},
+            {65, 56, 55, 52, 55, 62, 57}
+    };
+    HashSet notSeenResult =  Library.minMax(arr);
+
+    assertEquals(<67, 68, 69, 63>,notSeenResult);
+}
+
+
+
+@Test void RollTest(){
         int[] nRoll = {1,2,3,4,5,6};
         boolean flagRoll = false;
 
